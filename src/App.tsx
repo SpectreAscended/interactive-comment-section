077fee2a9bcdc64d23c40b861e3b1737c76f6c3c
Cory Pollard <astandard87@gmail.com>
2023-03-25 12:54:10 -0600

    Styled comment card

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-import Counter from './components/UI/Counter';
+import CommentCard from './components/CommentCard';
+import DUMMY_DATA from './store/DUMMY_DATA';
 
 const App: React.FC = () => {
+  const data = DUMMY_DATA;
+
   return (
-    <>
-      <div>App</div>
-      <Counter />
-    </>
+    <main>
+      {data.map(comment => {
+        return <CommentCard comment={comment} />;
+      })}
+    </main>
   );
 };
 
---
+++ src/components/CommentCard.tsx
@@ -0,0 +1,61 @@
+import { useState, useEffect } from 'react';
+import Counter from './UI/Counter';
+import './commentCard.scss';
+
+interface CommentCardProps {
+  comment: {
+    id: string;
+    content: string;
+    createdAt: string;
+    score: number;
+    user: {
+      image: {
+        png: string;
+        webp: string;
+      };
+      username: string;
+    };
+    replies: any;
+  };
+}
+
+const CommentCard: React.FC<CommentCardProps> = ({ comment }) => {
+  const [image, setImage] = useState();
+
+  useEffect(() => {
+    const fetchImage = async () => {
+      try {
+        const res = await import(`${comment.user.image.png}`);
+        console.log(res.default);
+        setImage(res.default);
+      } catch (err) {}
+    };
+    fetchImage();
+  }, []);
+
+  return (
+    <article className="comment-card">
+      <Counter />
+      <div className="comment-card__content">
+        <div className="comment-card__user-data">
+          <figure className="comment-card__user-data--img">
+            <img
+              src={image}
+              alt={`${comment.user.username} user image`}
+            />
+          </figure>
+          <span className="comment-card__user-data--username">
+            {comment.user.username}
+          </span>
+          <span className="comment-card__user-data--created-at">
+            {comment.createdAt}
+          </span>
+          <button className="comment-card__reply">Reply</button>
+        </div>
+        <p className="comment-card__comment-body">{comment.content}</p>
+      </div>
+    </article>
+  );
+};
+
+export default CommentCard;
