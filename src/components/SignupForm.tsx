af79754a759ef5d148ce449cc330bb0ac4b51308
Cory Pollard <astandard87@gmail.com>
2023-03-28 18:13:08 -0600

    Finished adding validation logic to signup form

--- src/components/SignupForm.tsx
+++ src/components/SignupForm.tsx
@@ -1,11 +1,11 @@
 import { Link } from 'react-router-dom';
 import Button from './UI/Button';
-import useValidation from '../hooks/useValidation';
+import useValidation, {
+  nameValidation,
+  emailValidation,
+  passwordValidation,
+} from '../hooks/useValidation';
 import './authForm.scss';
-
-const nameValidation = (input: string) => {
-  return input.trim().length > 0;
-};
 
 const SignupForm: React.FC = () => {
   const {
@@ -15,7 +15,34 @@ const SignupForm: React.FC = () => {
     hasError: nameHasError,
   } = useValidation(nameValidation);
 
-  console.log(nameHasError);
+  const {
+    inputValue: emailInputValue,
+    inputValueHandler: emailValueHandler,
+    inputBlurHandler: emailBlurHandler,
+    hasError: emailHasError,
+  } = useValidation(emailValidation);
+
+  const {
+    inputValue: passwordValue,
+    inputValueHandler: passwordValueHandler,
+    inputBlurHandler: passwordBlurHandler,
+    hasError: passwordHasError,
+  } = useValidation(passwordValidation);
+
+  const {
+    inputValue: confirmPasswordValue,
+    inputValueHandler: confirmPasswordValueHandler,
+    inputBlurHandler: confirmPasswordBlurHandler,
+    hasError: confirmPasswordHasError,
+  } = useValidation(passwordValidation);
+
+  const inputClassesHandler = (inputHasError: boolean) => {
+    return `auth-form__input ${inputHasError ? 'auth-form__input--error' : ''}`;
+  };
+  const nameClasses = inputClassesHandler(nameHasError);
+  const emailClasses = inputClassesHandler(emailHasError);
+  const passwordClasses = inputClassesHandler(passwordHasError);
+  const confirmPasswordClasses = inputClassesHandler(confirmPasswordHasError);
 
   return (
     <section>
@@ -28,11 +55,14 @@ const SignupForm: React.FC = () => {
           type="text"
           name="username"
           id="username"
-          className="auth-form__input"
+          className={nameClasses}
           value={nameInputValue}
           onChange={nameValueHandler}
           onBlur={nameBlurHandler}
         />
+        {nameHasError && (
+          <p className="auth-form__error-message">Username cannot be blank.</p>
+        )}
         <label htmlFor="email" className="auth-form__label">
           Email
         </label>
@@ -40,8 +70,17 @@ const SignupForm: React.FC = () => {
           type="email"
           name="email"
           id="email"
-          className="auth-form__input"
+          className={emailClasses}
+          value={emailInputValue}
+          onChange={emailValueHandler}
+          onBlur={emailBlurHandler}
         />
+        {emailHasError && (
+          <p className="auth-form__error-message">
+            Please enter a valid email.
+          </p>
+        )}
+
         <label htmlFor="password" className="auth-form__label">
           Password
         </label>
@@ -50,8 +89,17 @@ const SignupForm: React.FC = () => {
           autoComplete="current-password"
           name="password"
           id="password"
-          className="auth-form__input"
+          className={passwordClasses}
+          value={passwordValue}
+          onChange={passwordValueHandler}
+          onBlur={passwordBlurHandler}
         />
+        {passwordHasError && (
+          <p className="auth-form__error-message">
+            Password must be atleast 6 characters long.
+          </p>
+        )}
+
         <label htmlFor="password-confirm" className="auth-form__label">
           Confirm Password
         </label>
@@ -60,8 +108,16 @@ const SignupForm: React.FC = () => {
           autoComplete="current-password"
           name="password-confirm"
           id="password-confirm"
-          className="auth-form__input"
+          className={confirmPasswordClasses}
+          value={confirmPasswordValue}
+          onChange={confirmPasswordValueHandler}
+          onBlur={confirmPasswordBlurHandler}
         />
+        {confirmPasswordHasError && (
+          <p className="auth-form__error-message">
+            Password must be atleast 6 characters long.
+          </p>
+        )}
 
         <div className="auth-form__actions">
           <p>
--- src/hooks/useValidation.tsx
+++ src/hooks/useValidation.tsx
@@ -26,3 +26,19 @@ const useValidation = (validation: (a: string) => boolean) => {
 };
 
 export default useValidation;
+
+export const nameValidation = (input: string) => {
+  return input.trim().length > 0;
+};
+
+export const emailValidation = (input: string) => {
+  return (
+    input.trim().includes('@') &&
+    input.trim().includes('.') &&
+    input.trim().length > 4
+  );
+};
+
+export const passwordValidation = (input: string) => {
+  return input.trim().length >= 6;
+};
