ddd9dc246faf2931e7af4af41f57525c61e96139
Cory Pollard <astandard87@gmail.com>
2023-03-30 14:44:48 -0600

    Edited comment input to show users profile picture

--- src/components/CommentInput.tsx
+++ src/components/CommentInput.tsx
@@ -1,12 +1,27 @@
-import userImage from '../assets/avatars/image-juliusomo.png';
+import { useContext, useEffect, useState } from 'react';
+import { authContext } from '../context/AuthContext';
+// import userImage from '../assets/avatars/image-juliusomo.png';
 import Button from './UI/Button';
 import './commentInput.scss';
 
 const CommentInput: React.FC = () => {
+  const [userImg, setUserImg] = useState<any>(null);
+  const { userData } = useContext(authContext);
+
+  useEffect(() => {
+    if (userData) {
+      setUserImg(userData.userImage);
+    }
+  }, [userData]);
+
   return (
     <form className="comment-input">
       <figure className="comment-input__user-img">
-        <img src={userImage} alt="user img" />
+        {userData ? (
+          <img src={userImg} alt="user img" />
+        ) : (
+          <img src="null"></img>
+        )}
       </figure>
       <div className="comment-input__content">
         <textarea
