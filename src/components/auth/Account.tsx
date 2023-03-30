7b37b48447f6fea517df4a1778ec7e03b5967253
Cory Pollard <astandard87@gmail.com>
2023-03-30 13:30:43 -0600

    Adjusted styling for uploaded image

--- src/components/auth/Account.tsx
+++ src/components/auth/Account.tsx
@@ -1,27 +1,35 @@
 import { authContext } from '../../context/AuthContext';
 import { useContext, useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { storage } from '../../firebase';
+import { storage, auth } from '../../firebase';
 import { ref, uploadBytes, listAll, getDownloadURL } from 'firebase/storage';
 import './account.scss';
+import { updateProfile } from 'firebase/auth';
 
 const Account: React.FC = () => {
   const { userData } = useContext(authContext);
   const [imageUpload, setImageUpload] = useState<any>(null);
-  const [imageList, setImageList] = useState<string[]>([]);
+  const [img, setImg] = useState<string | null>(null);
+  const uid = userData.uid;
 
-  const imagesListRef = ref(storage, 'images/');
+  const imagesListRef = ref(storage, `users/${uid}/userImage/`);
   const uploadImage = async (e: React.FormEvent) => {
     e.preventDefault();
     if (imageUpload == null) return;
     const imageRef = ref(
       storage,
-      `images/${crypto.randomUUID() + imageUpload.name}`
+      `users/${uid}/userImage/${crypto.randomUUID() + imageUpload.name}`
     );
     try {
       const snapshot = await uploadBytes(imageRef, imageUpload);
       const url = await getDownloadURL(snapshot.ref);
-      setImageList(prev => [...prev, url]);
+      setImg(url);
+
+      const user = auth.currentUser;
+      if (user) {
+        const foo = await updateProfile(user, { photoURL: url });
+        console.log('foo', foo);
+      }
     } catch (err) {}
   };
   const imageUploadHandler = (e: React.FormEvent<HTMLInputElement>) => {
@@ -29,21 +37,25 @@ const Account: React.FC = () => {
       setImageUpload(e.currentTarget.files[0]);
     }
   };
-  console.log(imageList);
+  console.log(auth.currentUser);
   useEffect(() => {
-    listAll(imagesListRef).then(res => {
-      res.items.forEach(item => {
-        getDownloadURL(item).then(url => {
-          setImageList(prevList => [...prevList, url]);
-        });
-      });
-    });
-  }, []);
+    // listAll(imagesListRef).then(res => {
+    //   res.items.forEach(item => {
+    //     getDownloadURL(item).then(url => {
+    //       // setImageList(prevList => [...prevList, url]);
+    //     });
+    //   });
+    // });
+    setImg(auth.currentUser?.photoURL!);
+  }, [img]);
 
   return (
     <section className="account">
       <h1 className="account__heading">Edit Account</h1>
       <h2>{userData.userName}</h2>
+      <figure className="account__settings--userImg">
+        {img ? <img src={img} alt="" className="image" /> : ''}
+      </figure>
       <div className="account__settings">
         <ul className="account__settings-list">
           <li className="account__settings-list--item">
