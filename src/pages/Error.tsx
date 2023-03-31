ace87fe2e582445c5fcb723bec5350a6aad57af4
Cory Pollard <astandard87@gmail.com>
2023-03-30 22:22:10 -0600

    Added userouteerror to load error data

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,11 +1,37 @@
 import Header from '../components/Header';
+import { useRouteError } from 'react-router';
+import './error.scss';
+
+interface Error {
+  message: string;
+  status: number;
+  data: {
+    message: string;
+  };
+}
+
 const ErrorBoundary: React.FC = () => {
+  const error = useRouteError() as Error;
+
+  let title = 'An error occured!';
+  let message = 'Something went wrong!';
+
+  if (error.status === 500) {
+    message = error.data.message;
+  }
+
+  if (error.status === 404) {
+    title = 'Not found!';
+    message = 'Could not find resource or page.';
+  }
+
   return (
     <>
       <Header />
       <main>
         <section className="error">
-          <h1 className="error__heading">Something went wrong</h1>
+          <h1 className="error__heading">{title}</h1>
+          <p>{message}</p>
         </section>
       </main>
     </>
